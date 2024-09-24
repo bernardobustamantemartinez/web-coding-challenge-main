c1ea3961139d03f22670d7ce48e03437261561b1
Bernardo <bernardobustamantemartinez@gmail.com>
2024-09-24 16:58:18 -0700

    Styling

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./TodoForm.css";
 
 function TodoForm({ addTodo }) {
   const [text, setText] = useState("");
@@ -11,14 +12,17 @@ function TodoForm({ addTodo }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="todo-form">
       <input
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Add a new todo"
+        className="todo-input"
       />
-      <button type="submit">Add</button>
+      <button type="submit" className="add-button">
+        Add
+      </button>
     </form>
   );
 }
--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,17 +1,23 @@
+import "./TodoItem.css";
+
 function TodoItem({ todo, toggleTodo, deleteTodo }) {
   return (
-    <li>
+    <li className="todo-item">
       <input
         type="checkbox"
         checked={todo.completed}
         onChange={() => toggleTodo(todo.id)}
+        className="todo-checkbox"
       />
       <span
+        className="todo-text"
         style={{ textDecoration: todo.completed ? "line-through" : "none" }}
       >
         {todo.text}
       </span>
-      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+      <button onClick={() => deleteTodo(todo.id)} className="delete-button">
+        Delete
+      </button>
     </li>
   );
 }
