848e93b5276b855551cfe00db3494bef3e6a6b51
Bernardo <bernardobustamantemartinez@gmail.com>
2024-09-25 01:46:34 -0700

    Styling fixes

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -23,14 +23,11 @@ function TodoItem({ todo, toggleTodo, deleteTodo, togglePriority }) {
       </span>
       <button
         onClick={() => togglePriority(todo.id)}
-        className="ml-2 px-2 py-1 bg-yellow-500 text-white rounded hover:bg-yellow-600"
+        className="priority-button"
       >
         {todo.priority ? "Unprioritize" : "Prioritize"}
       </button>
-      <button
-        onClick={() => deleteTodo(todo.id)}
-        className="ml-2 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
-      >
+      <button onClick={() => deleteTodo(todo.id)} className="delete-button">
         Delete
       </button>
     </li>
